380c9d5736e2442eeff785aafd9a8181b1a0b321
Dennis Durairaj <dennis.durairaj@gmail.com>
2017-12-20 13:11:13 +0530

    mlab setup

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import express from "express";
 import path from "path";
 import bodyParser from "body-parser";
 import cors from "cors";
-
+import mongo from "mongodb";
+import monk from 'monk';
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+const db = monk("mongodb://dennis:dennis@ds121336.mlab.com:21336/bookstore")
+
+app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(bodyParser.json());
 app.use(cors({ origin: "http://localhost:3000" }));
+app.use(function(req,res,next){
+  req.db = db;
+  next();
+});
 
 function fibonacci() {
   var a = 1,
@@ -30,6 +37,14 @@ app.get("/api/fibonacci", (req, res) => {
   console.log(result);
   res.status(200).json({ result });
 });
+
+app.get("/api/bookstore", (req, res) => {
+  var db = req.db;
+  var collection = db.get('books');
+  collection.find({},{},function(e,docs){
+      res.send(docs);
+  });
+})
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
